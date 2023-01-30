4cdcf200fb8416813a5244caa74330ab62718911
berat03 <bbulbulwork@gmail.com>
2023-01-30 21:42:26 +0000

    comments cleaned and added

--- main.c
+++ main.c
@@ -8,30 +8,29 @@ int main(int argc,  char *argv[]){
     struct ant *ant_p;
     struct ant bob;
     ant_p = &bob;
-
+    // use argv[1] so that is user inputs two rules or any second input (separate by a space) it will only read the original rule
     if(argv[1]){ //if we have an input
         char input_rules[] = "LR";
-        if (strspn(argv[1], input_rules) != strlen(argv[1])) { //True if invalid input
-            printf("Invalid input\n"); //do `I print this?
+        if (strspn(argv[1], input_rules) != strlen(argv[1])) { //TRUE if invalid input
+            printf("Invalid input\n");
             return 1;
         }
-
         struct rule general_rule;
         struct rule * general_rule_p;
-        general_rule.rules = malloc(sizeof(char) * (strlen(argv[1]) + 1));
-        if (general_rule.rules == NULL) { // if malloc fails
+        general_rule.rules = malloc(sizeof(char) * (strlen(argv[1]) + 1)); //Allocating memory w.r.t length of rule
+        if (general_rule.rules == NULL) { // if malloc fails general_rule.rules will return NULL
             printf("Error: malloc() failed.\n");
             return 1;
         }
         strcpy(general_rule.rules, argv[1]);
         general_rule_p = &general_rule;
 
-        // general version here
+        // general version
         start_visualisation(ant_p);
         while (not_quit())
             visualise_and_advance_general(ant_p, general_rule_p);
-        end_visualisation();
-        free(general_rule.rules);
+        end_visualisation(); // could not have to repeat this code using if else, but better readability*
+        free(general_rule.rules); // to ensure no memory leak
         return 0;
     }
 
